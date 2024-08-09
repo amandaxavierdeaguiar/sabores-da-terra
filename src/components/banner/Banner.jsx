c9047a9a2e1a7389c76b38582186e665e53993eb
amandaxavierdeaguiar <amandaxavierdeaguiar@gmail.com>
2024-08-09 16:59:45 +0100

    alteracao banner

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,8 +1,8 @@
 import {Carousel, Image} from 'react-bootstrap'
 import './banner.css'
-import banner1 from 'src/assets/banner/banner1.png';
-import banner2 from 'src/assets/banner/banner2.png';
-import banner3 from 'src/assets/banner/banner3.png';
+import banner1 from '../../assets/banner/banner1.png';
+import banner2 from '../../assets/banner/banner2.png';
+import banner3 from '../../assets/banner/banner3.png';
 
 function Banner(){
     /*const banner1 = 'src/assets/banner/banner1.png'
