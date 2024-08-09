3e042b4fe2a95945b248c401100ef064775fd67f
amandaxavierdeaguiar <amandaxavierdeaguiar@gmail.com>
2024-08-09 17:23:39 +0100

    banner ajustes

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,5 +1,5 @@
 import {Carousel, Image} from 'react-bootstrap'
-import '../banner.css'
+import './banner.css'
 import banner1 from '../../assets/banner/banner1.png';
 import banner2 from '../../assets/banner/banner2.png';
 import banner3 from '../../assets/banner/banner3.png';
